647c62f4ff0505321536644bade081efb1d71bc9
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-25 13:10:24 -0300

    Added env vars to load API url in proyecto running in development

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  address: {
+  	spa: '"http://localhost:8080/"',
+  	api: '"http://localhost:3000/api/"'
+  }
 })

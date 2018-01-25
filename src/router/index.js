bf28a47c2bfabcb778467240ea3d464b4f7e15ad
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-25 13:13:31 -0300

    Added route for breed resource

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Breeds from '@/components/Breed'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'wolves-index',
       component: HelloWorld
+    },
+    {
+      path: '/breeds',
+      name: 'breeds',
+      component: Breeds
     }
   ]
 })

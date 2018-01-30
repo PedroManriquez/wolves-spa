1da71787c9e30d63154f2b9a7ef7b88a0bd94a42
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-30 20:26:02 -0300

    Fix style and change img

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="logo">
+      <img src="/static/wolf.png">
+    </div>
     <router-view/>
   </div>
 </template>
@@ -16,8 +18,14 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2em;
+}
+.logo {
+  text-align: center;
+  margin-bottom: 0.8em;
+}
+.logo img {
+  width: 15em;
 }
 </style>

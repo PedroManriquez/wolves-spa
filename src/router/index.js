a74c2c61c3eda3fa4f6b3a5baae026d804008dc0
PedroManriquez <ptrmanriquez@gmail.com>
2018-01-30 20:25:24 -0300

    Delete unnecesary files invoked

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Breeds from '@/components/Breed/Breed'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'wolves-index',
-      component: HelloWorld
+      redirect: {name: 'breeds'}
     },
     {
       path: '/breeds',
